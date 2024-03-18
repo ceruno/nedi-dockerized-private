357d68a77603d8526c4420a9790fa0b6d757eede
Andy Kruesi <andy@krue.si>
2024-03-18 23:00:50 +0100

    fix docker-entrypoint.sh

--- nedi/docker-entrypoint.sh
+++ nedi/docker-entrypoint.sh
@@ -18,7 +18,7 @@ fi
 
 # Move Nedi app files to volume
 if [ ! -e $NEDI_FOLDER/nedi.conf ]; then
-    cp /tmp/nedi $NEDI_FOLDER
+    cp --verbose -r /tmp/nedi/ $NEDI_FOLDER/
     chown -R www-data:www-data $NEDI_FOLDER
 fi
 
