c3b1e21244858e65546506a8c705ee020e532539
Andy Kruesi <andy@krue.si>
2024-03-18 20:44:51 +0100

    add volume mapping for nedi service

--- nedi/docker-entrypoint.sh
+++ nedi/docker-entrypoint.sh
@@ -1,16 +1,33 @@
 #!/bin/sh
 
+# Wait until database is ready
+while ! mysqladmin ping -h"$NEDI_DB_HOST" --silent; do sleep 1; done
+
 # Create DB
-sleep 60
-# sed -i 's#dbpa55#'${NEDI_DB_PASS}'#g' /var/nedi/nedi.conf
-DB_ALREADY_INITIALIZED="/var/nedi/db-status/use_existing_database.pid"
-if [ ! -e $DB_ALREADY_INITIALIZED ]; then
-    touch $DB_ALREADY_INITIALIZED
-    echo "-- First container startup --"
+NEDI_FOLDER="var/nedi"
+DB_INIT_FILE="$NEDI_FOLDER/mapped_files/db_initialized.pid"
+NEDI_CONF="$NEDI_FOLDER/mapped_files/nedi.conf"
+NEDI_CRON="$NEDI_FOLDER/mapped_files/crontab"
+
+if [ ! -e $DB_INIT_FILE ]; then
+    touch $DB_INIT_FILE
+    echo "First container startup! Initialize new DB..."
     # YOUR_JUST_ONCE_LOGIC_HERE
     expect /var/nedi/install.exp $NEDI_DB_USER $NEDI_DB_PASS
 else
     echo "DB is already initialized."
+fi
+
+if [ ! -e $NEDI_CONF ]; then
+    mv /var/nedi/nedi.conf $NEDI_CONF
+    ln -s NEDI_CONF /var/nedi/nedi.conf
+    chown -R www-data:www-data $NEDI_FOLDER
+fi
+
+if [ ! -e $NEDI_CRON ]; then
+    mv /var/nedi/inc/crontab $NEDI_CRON
+    ln -s NEDI_CRON /var/nedi/inc/crontab
+    chown -R www-data:www-data $NEDI_FOLDER
 fi
 
 /usr/local/sbin/php-fpm
